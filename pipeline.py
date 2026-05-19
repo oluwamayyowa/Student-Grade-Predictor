2dc8951ca63be08cdd084ffc9e0f8ebe789d7ff6
Mayowa <oluwamayyowa@gmail.com>
2026-05-19 13:55:33 -0400

    - extraction 4 my pipeline

---
+++ pipeline.py
@@ -0,0 +1,6 @@
+# modules and imports
+import pandas as pd
+
+# Extract | using pandas to read the csv files
+extract = pd.read_csv("Student-Academic-Performance-Survey(Sheet1).csv", encoding='latin1')
+# print(extract)
